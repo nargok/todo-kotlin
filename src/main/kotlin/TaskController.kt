c9ae5f75a19f47a3b2ec09761da41891e8d1d54f
後閑諒一 <nargok@gmail.com>
2017-10-26 00:42:42 +0900

    create action

--- src/main/kotlin/TaskController.kt
+++ src/main/kotlin/TaskController.kt
@@ -1,10 +1,22 @@
 package todolist
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import spark.Route
+import spark.Spark.halt
 
-class TaskController(private val taskRepository: TaskRepository) {
+class TaskController(private val objectMapper: ObjectMapper,
+                     private val taskRepository: TaskRepository) {
 
     fun index(): Route = Route { req, res ->
         taskRepository.findAll()
     }
+
+    fun create(): Route = Route { req, res ->
+        val request: TaskCreateRequest =
+                objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400) // 型：TaskCreateRequestを推論する
+        val task = taskRepository.create(request.content)
+        res.status(201)
+        task
+    }
+
 }
